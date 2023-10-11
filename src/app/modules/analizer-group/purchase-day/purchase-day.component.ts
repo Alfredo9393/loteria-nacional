084d3e076de647f9d54e65190bd6104474fdf9a9
Alfredo9393 <alfredo_9393@live.com.mx>
2023-10-11 03:11:49 -0600

    activar de color el dia que se hizo la compra

--- src/app/modules/analizer-group/purchase-day/purchase-day.component.ts
+++ src/app/modules/analizer-group/purchase-day/purchase-day.component.ts
@@ -2,6 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import * as moment from 'moment';
 import { HttpClient } from '@angular/common/http'; 
 
+export class ElementDt { //interface
+  index: string;
+  numero: number;
+  name: string;
+  dias_transcurridos: number;
+  fechaInicio: string;
+  fechaFin: string;
+  datePurchase: string;
+  activePurchase: boolean;
+  arrayDays:ElementDay[] ;
+}
+
+export class ElementDay {
+  day: number;
+  value:string;
+  active:boolean;
+}
+
 @Component({
   selector: 'app-purchase-day',
   templateUrl: './purchase-day.component.html',
@@ -24,10 +42,10 @@ export class PurchaseDayComponent  {
 
   dataExt: any[] = [];
   dataAux: any;
-  dataFull: any[] = [];
 
   header: Array<string> = ['Index', 'numero', 'nombre', 'Fecha Inicio'];
 
+  arrayData:ElementDt[] =[];
 
   constructor(private http: HttpClient) {
     this.loadData();
@@ -40,53 +58,65 @@ export class PurchaseDayComponent  {
       this.dataAux = data;
 
         for (const line of this.dataAux.split(/[\r\n]+/)){
-          // console.log("next>>>>>");
 
           var dataNoSpace= line.replace(/\s/g, "");
           var str_array = dataNoSpace.split(',');
           // console.log(str_array);
 
-          var index =str_array[0];
-          var numero =str_array[1];
-          var nombre =str_array[2];
-          var fechaInicio =str_array[3];
-          var fechaFin =str_array[4];
 
-          var fecha1 = moment(fechaInicio);
-          var fecha2 = moment(fechaFin);
-          var diferencia=fecha2.diff(fecha1, 'days')
-
-          // console.log(fecha2.diff(fecha1, 'days'), ' dias de diferencia -' , nombre);
+          // 
           
-          var index =str_array[0];
-          var numero =str_array[1];
-          var nombre =str_array[2];
-          var fechaInicio =str_array[3];
-          var fechaFin =str_array[4];
+          var dateStart =str_array[3];
+          var dateEnd =str_array[4];
+          var diferencia = moment(dateEnd).diff(moment(dateStart), 'days');
 
-          let objs = {}; 
-          objs['index'] = index
-          objs['numero'] = numero
-          objs['name'] = nombre
-          objs['dias_transcurridos'] = diferencia
-          objs['fechaInicio'] = fechaInicio
+
+          const element: ElementDt = new ElementDt();
+          element.index=str_array[0];
+          element.numero=str_array[1];
+          element.name=str_array[2];
+          element.fechaInicio=dateStart;
+          element.fechaFin=dateEnd;
+          element.dias_transcurridos=diferencia;
+          element.activePurchase=false;
+
+          element.datePurchase=str_array[5] !== undefined ? moment(str_array[5]).format('YYYY-MM-DD') : "" ;
+
+          
+
+
+          // element.datePurchase=moment(str_array[5]).format('DD-MM-YYYY');
+
+        console.log("index: ["+element.index+"] name", element.name, +" datePurchase: "+ element.datePurchase + " str_array[5]: "+str_array[5] );
+          let arrayDays:any[] =[];
 
           for(let i = 0; i <= diferencia; i++) {
-            var new_date = moment(fecha1, "DD-MM-YYYY").add(i, 'days');
+
+            var new_date = moment(element.fechaInicio, "YYYY-MM-DD").add(i, 'days');
             var day = new_date.format('DD');
             var month = new_date.format('MM');
             var year = new_date.format('YYYY');
-            objs['day' + i] = day + '-' + month + '-' + year
+            var new_date_fort = year + '-' + month + '-' + day
 
+            const elementDay: ElementDay = new ElementDay();
+
+            elementDay.day=i;
+            elementDay.value=new_date_fort;
+            if (elementDay.value ==  element.datePurchase ){
+              console.log(" match: "+ elementDay.value  +" == "+ element.datePurchase )
+              elementDay.active = true;
+            }
+            arrayDays.push(elementDay)
+            
             //Agregar los dias en el Header de la tabla
             let name_header= 'day ' + i
             this.header.indexOf(name_header) === -1 ? this.header.push(name_header) : console.log("This header already exists") ;
 
-          }
-          objs['fechaFin'] = fechaFin
-          
 
-          this.dataFull.push( objs )
+          }
+          element.arrayDays=arrayDays;
+          this.arrayData.push(element);
+
 
       }
 
@@ -94,7 +124,9 @@ export class PurchaseDayComponent  {
       this.header.push("fechaFin")
 
 
-      console.log(this.dataFull);
+      // this.activeDatePurchase();
+
+      console.log(this.arrayData);
       console.log(this.header);
 
       this.processData();
@@ -103,22 +135,37 @@ export class PurchaseDayComponent  {
 
   }
 
+//  activeDatePurchase(){
+//     this.arrayData.forEach( (element) => {
+//       console.log("next>>>>>",element.name); 
+
+//       for(let i = 0; i <= element.dias_transcurridos; i++) {
+//         // console.log("dias ",element['day'+i]); 
+//           if (element['day'+i] == element.datePurchase ){
+//               // console.log(" match: "+ moment(new_date).format('YYYY-MM-DD') +" == "+ moment(fechaCompra).format('YYYY-MM-DD') )
+//               console.log(" match: "+ element['day'+i] +" == "+ element.datePurchase )
+//               element.activePurchase = true
+//           }
+//       }
+
+//     });
+//  }
 
   private processData() {
     const statesSeen = {};
     const countiesSeen = {};
 
-    this.dataExt = this.dataFull.sort((a, b) => {
+    this.dataExt = this.arrayData.sort((a, b) => {
       const stateComp = a.index.localeCompare(b.index);
       return stateComp ? stateComp : a.name.localeCompare(b.name);
     }).map(x => {
       const stateSpan = statesSeen[x.index] ? 0 :
-        this.dataFull.filter(y => y.index === x.index).length;
+        this.arrayData.filter(y => y.index === x.index).length;
 
       statesSeen[x.index] = true;
 
       const countySpan = countiesSeen[x.index] && countiesSeen[x.index][x.name] ? 0 :
-        this.dataFull.filter(y => y.index === x.index && y.name === x.name).length;
+        this.arrayData.filter(y => y.index === x.index && y.name === x.name).length;
 
       countiesSeen[x.index] = countiesSeen[x.index] || {};
       countiesSeen[x.index][x.name] = true;
